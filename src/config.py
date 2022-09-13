3d68f05573e721f39148cc3b23f10ef4b7047cda
cerion <cerion42@gmail.com>
2022-09-13 14:49:21 +0200

    removed tracking of config file

--- src/config.py
+++
@@ -1,2 +0,0 @@
-api_key = "YOUR OPENAI API KEY" #The key you get from https://beta.openai.com/account/api-keys
-voice_number = 0                 #The default voice number you want to use on your system
